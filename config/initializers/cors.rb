1aef383f77a05d0e59a1cd2eac8c98cf82e70fd5
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-02-02 15:44:39 +0300

    Fix cors errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /http:\/\/(.*?)\.example\.com/
+    origins "https://restro-api.onrender.com"
 
     resource "*",
       headers: :any,
