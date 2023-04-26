ad595b511bf5edaea240af3faee8df1abf450252
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-26 13:02:32 +0300

    Fix url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://restrofinder.vercel.app/"
+    origins "https://restrofinder.vercel.app"
 
     resource "*",
       headers: :any,
