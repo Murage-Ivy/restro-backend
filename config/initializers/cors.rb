cc1a8638c504fa200daf79307f9e703bcbae137b
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-26 13:12:36 +0300

    allow reviews requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,5 +14,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:create, :get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
+
+    resource "/reviews",
+             headers: :any,
+             methods: [:create, :get, :post, :put, :patch, :delete, :options, :head],
+             credentials: true
   end
 end
